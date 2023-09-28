e219f007d97910aa29237ac5814fff33dc053290
mihaelacsn <74179246+mihaelacsn@users.noreply.github.com>
2023-09-28 22:39:41 +0300

    Update homework.py

--- WebScarper/homework.py
+++ WebScarper/homework.py
@@ -1,29 +1,27 @@
+import requests
 from bs4 import BeautifulSoup
 import json
-import requests
 
-def scrape_url(url):
+def scrape_ad_info(url):
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
 
-    item = {
+    ad_info = {
         "page_title": soup.select_one("header.adPage__header h1").text.strip(),
         "price": soup.select_one("span.adPage__content__price-feature__prices__price__value").text.strip(),
         "update_time": soup.select_one("div.adPage__aside__stats__date").text.strip(),
         "balcony": soup.select_one("span.adPage__content__features__value").text.strip(),
-   
     }
 
-    CHARACTERISTICS.append(item)
+    return ad_info
 
 urls = ["https://999.md/ro/74323770"]
-
-
 FILENAME = "info.json"
-CHARACTERISTICS = []
+ad_info_list = []
 
 for url in urls:
-    scrape_url(url)
+    ad_info = scrape_ad_info(url)
+    ad_info_list.append(ad_info)
 
 with open(FILENAME, "w", encoding="utf-8") as file:
-    json.dump(CHARACTERISTICS, file, indent=4, ensure_ascii=False)
+    json.dump(ad_info_list, file, indent=4, ensure_ascii=False)
